7bb3b4b5e89db31e10f240e18c88ae0428c21cb5
Pratyush Sureka <pratsure@gmail.com>
2019-05-04 22:32:52 +0530

    -

--- compton_tryone/install.sh
+++ compton_tryone/install.sh
@@ -1,21 +1,13 @@
 #!/usr/bin/env bash
 set -e 
 
-now=`pwd`
-#sudo xbps-install -S `cat dep_p`
-if [[ -f ~/.packages ]]; then
-  cd ~/.packages/compton
-  git clone https://github.com/tryone144/compton.git
-else
-  mkdir -p ~/.packages
-  cd ~/.packages/compton
-  git clone https://github.com/tryone144/compton.git
-fi
+# sudo xbps-install -S `cat dep_p`
+git clone https://github.com/tryone144/compton.git
+
+cd compton
 
 make
 make docs
 sudo make install
-
-cd $now
 
 exit
