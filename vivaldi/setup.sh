b5b07e4884b91c483269cdf283eedb99fa52680c
Pratyush Sureka <pratsure@gmail.com>
2019-11-21 10:02:29 +0530

    -

---
+++ vivaldi/setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+wget https://gist.github.com/ruario/b5aca855f2d05ba14836/archive/1a0c14c6fac79fd3f40d7e58199c6431ce71c4fc.zip
+unzip 1a0c14c6fac79fd3f40d7e58199c6431ce71c4fc.zip
+cd *
+bash install-vivaldi.sh
+
